8665201befee24fcc9f27024562146db012f886c
Matevarzma <mmilorava73@gmail.com>
2026-05-21 14:49:21 -0400

    updated contact infromation

--- car-parts-app/client/src/components/Footer.jsx
+++ car-parts-app/client/src/components/Footer.jsx
@@ -4,14 +4,21 @@ function Footer() {
       <div className="container footer-grid">
         <div>
           <h3>კონტაქტი</h3>
-          <p>ტელ: +995 000 000 000</p>
-          <p>ელფოსტა: vitiparts@example.com</p>
+          <p>ტელ: +995 555 165 145</p>
         </div>
 
         <div>
           <h3>სოციალური ქსელები</h3>
-          <p>Facebook: Auto Shine</p>
-          <p>Instagram: Auto Shine</p>
+          <p>
+            Facebook:{" "}
+            <a
+              href="https://www.facebook.com/mersedesis.aksesuarebi.131296"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Viti Parts
+            </a>
+          </p>
         </div>
       </div>
     </footer>
